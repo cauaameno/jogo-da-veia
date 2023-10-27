10bc8fee296edc38ee3cc06740d5fa59763a6932
cauaameno <106753236+cauaameno@users.noreply.github.com>
2023-10-27 10:48:34 -0300

    jogo

---
+++ jogo.js
@@ -0,0 +1,9 @@
+//seleciona todos os elementos com a classe "celula" e os armazenar 
+const celulas = document.querySelectorAll(".celula");
+
+let checarTurbe =true;
+
+let turno;
+
+const jogador_X = "X";
+const jogador_0 = "0";
